e6ee77dfa1266a3721f0c1f3443704b6219fcacf
VovaSewer <shutko7@gmail.com>
2022-02-09 13:31:43 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,17 +2,18 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';  
 
-const refs = {
-days: document.querySelector('[data-days]'),
-hours: document.querySelector('[data-hours]'),
-minutes: document.querySelector('[data-minutes]'),
-seconds: document.querySelector('[data-seconds]'),
+let refs = {
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
+    startBtn: document.querySelector('button[data-start]'),
+    dateSelector: document.querySelector('#datetime-picker'),
 };
 
-const startBtn = document.querySelector('button[data-start]');
-const selector = document.querySelector('input#datetime-picker');
-
-flatpickr(selector, options);
+refs.startBtn.disabled = true;
+let timerId = null;
+let startTimer = new Date();
 
 const options = {
     enableTime: true,
@@ -21,48 +22,28 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
       console.log(selectedDates[0]);
+      
+      if (selectedDates[0] < Date.now()) {
+          window.alert (`Please choose a date in the future`)
+      } else {
+          refs.startBtn.disabled = false;
+          startTimer = Date.parse(selectedDates[0]) - Date.now();
+        //   console.log(startTimer);
+          updateTimer();
+      }
     },
   };
 
+  flatpickr(refs.dateSelector, options);
 
+ refs.startBtn.addEventListener('click', () => {
+     timerId = setInterval(() => {
 
-const timer = {
-    // intervalId: null,
-    // isActive: false,
-    start() {
-        // if(this.isActive){
-        //     return;
-        // }
-        const startTime = Date.now(); 
-        // this.isActive = true;
-        // this.intervalId = 
-        setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = currentTime - startTime;
-            const time = convertMs(deltaTime);
-            
-            updateClockFace(time);
-        }, 1000);
-    },
-};
-
-
-startBtn.addEventListener ('click', ()=>{
-    timer.start();
+        updateTimer();
+    }, 1000);
 });
 
 
-
-function updateClockFace ({days, hours, minutes, seconds}){
-    refs.days.textContent = `${days}`;
-    refs.hours.textContent = `${hours}`;
-    refs.minutes.textContent = `${minutes}`;
-    refs.seconds.textContent = `${seconds}`;
-}
-
-function pad(value) {
-    return String(value).padStart(2, '0');
-}
 
 function convertMs(ms) {
     // Number of milliseconds per unit of time
@@ -71,15 +52,35 @@ function convertMs(ms) {
     const hour = minute * 60;
     const day = hour * 24;
   
-
-    const days = pad(Math.floor(ms / day));
-    const hours = pad(Math.floor((ms % day) / hour));
-    const minutes = pad(Math.floor(((ms % day) % hour) / minute)); 
-    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+    const days = addLeadingZero(Math.floor(ms / day));
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute)); 
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
   
     return { days, hours, minutes, seconds };
   }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
+
+  function updateTimer() {
+    if (startTimer > 1) {
+        let formatedTime = convertMs(startTimer);
+        // console.log(formatedTime);
+        refs.days.textContent = addLeadingZero(formatedTime.days);
+        refs.hours.textContent = addLeadingZero(formatedTime.hours);
+        refs.minutes.textContent = addLeadingZero(formatedTime.minutes);
+        refs.seconds.textContent = addLeadingZero(formatedTime.seconds);
+    } else {
+        clearInterval(timerId);
+    }
+    startTimer -= 1000;
+}
+
+
+
+//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+//   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
